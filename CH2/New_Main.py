57d973b72010b36ca90f66eda1a250ea190137ae
Vincent Mills <85770762+MotoVince@users.noreply.github.com>
2022-06-16 08:34:34 -0400

    New Overdraft program, using Functions.
    So I started to learn functions in codecademy, definitely is what this program needs right now rather then trying to import modules across files. 
    Still needs some debugging. When the Main program enters the while loop it's supposed to exit when a balance of 0 is inputted by the user. However it is not currently doing so. Hopefully get more time to debug on my lunch break later today.

---
+++ CH2/New_Main.py
@@ -0,0 +1,31 @@
+balance = 0
+overdrawn = 0
+fee = 0
+balance_prompt = "Please Enter Account Balance "
+overdraft_prompt = "How Many Times has the Account been Overdrawn? "
+fee_prompt = "Overdraft Fee for this Account is: "
+
+#Detail:
+def overdraft_fee(overdrawn, balance):
+  return (balance * 0.01) + 5 * overdrawn
+  
+
+#Housekeeping:
+def housekeeping():
+  print(balance_prompt)
+  balance = int(input())
+  return balance 
+
+#End of job:
+def end_of_job():
+  print("Thank You for using this program, have a great day.")
+
+#Main:
+
+housekeeping()
+while balance >= 0:
+  fee = overdraft_fee(int(input(overdraft_prompt)), balance)
+  print(fee_prompt, fee)
+  housekeeping()
+else:
+  end_of_job()
